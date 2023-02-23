50eb1a2371d8cf2984f8613fb991da94f681ee56
Ekaterina Davydenko <lodeim@MacBook-Air-Alexander.local>
2023-02-23 23:09:22 +0300

    base layout

---
+++ src/MainPage/index.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+
+const MainPage = () =>{
+    return (
+        <Layout nickName='Test User' id='1' >
+            <div>main page</div>
+        </Layout>
+        
+    )
+}
+
+export default MainPage
